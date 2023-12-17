eca7b1b066a37fe08d656ead11d9aa6dd0af84d4
Rafa Fajari <rafafajari230907@gmail.com>
2023-12-18 00:14:43 +0700

    beres

---
+++ app/Http/Controllers/mahasiswaController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\mahasiswa;
+use Illuminate\Support\Facades\Session;
+
+class mahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $katakunci = $request->katakunci;
+        $jumlahbaris = 10;
+        if (strlen($katakunci)) {
+            $data = mahasiswa::where('nim', 'like', "$katakunci")->orWhere('nama', 'like', "%$katakunci%")->orWhere('jurusan', 'like', "%$katakunci%")->paginate($jumlahbaris);
+        } else {
+
+        }
+        $data = mahasiswa::orderBy('nim', 'desc')->paginate($jumlahbaris);
+        return view('mahasiswa.index')->with('data', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Session::flash('nim', $request->nim);
+        Session::flash('nama', $request->nama);
+        Session::flash('jurusan', $request->jurusan);
+
+        $request->validate([
+            'nim' => 'required|numeric|unique:mahasiswa,nim',
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ], [
+            'nim.required' => 'NIM wajib diisi',
+            'nim.numeric' => 'Orang gila nimnya huruf',
+            'nim.unique' => 'NIM sudah terdaftar',
+            'nama.required' => 'Nama wajib diisi',
+            'jurusan.required' => 'Jurusan wajib diisi',
+        ]);
+        $data = [
+            'nim' => $request->nim,
+            'nama' => $request->nama,
+            'jurusan' => $request->jurusan,
+        ];
+        mahasiswa::create($data);
+        return redirect()->to('mahasiswa')->with('success', 'Data berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data = mahasiswa::where('nim', $id)->first();
+        return view('mahasiswa.edit')->with('data', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ], [
+            'nama.required' => 'Nama wajib diisi',
+            'jurusan.required' => 'Jurusan wajib diisi',
+        ]);
+        $data = [
+            'nama' => $request->nama,
+            'jurusan' => $request->jurusan,
+        ];
+        mahasiswa::where('nim', $id)->update($data);
+        return redirect()->to('mahasiswa')->with('success', 'Data berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        mahasiswa::where('nim', $id)->delete();
+        return redirect('mahasiswa')->with('success', 'Berhasil menghapus data');
+    }
+}
